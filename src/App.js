868663135c81758eee8ec45b8f9846919509a733
scudetti-guillaume <124565246+scudetti-guillaume@users.noreply.github.com>
2023-02-26 20:01:13 +0100

    test

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='maybar/carte' element={<Carte />} />
-          <Route path='maybar/contact' element={<Contact />} />
-          <Route path='maybar/condition' element={<Conditions />} />
-          <Route path='maybar/*' element={<NotFound />} />
+          <Route path='/maybar/carte' element={<Carte />} />
+          <Route path='/maybar/contact' element={<Contact />} />
+          <Route path='/maybar/condition' element={<Conditions />} />
+          <Route path='/maybar/*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
